61136c935669cca5eb9d1892121ecb2a9dc72702
RomarioNaiana <romarionaiana@gmail.com>
2025-07-23 17:29:36 +0000

    fim

---
+++ models/calculadora.js
@@ -0,0 +1,10 @@
+const calculadora={
+  subtracao: function(n1, n2){
+    return n1 - n2
+  },
+  soma: function(n2, n3){
+    return n2 + n3
+  }
+}
+
+module.exports = calculadora
--- tests/caculadora.test.js
+++ tests/caculadora.test.js
@@ -1,8 +1,19 @@
 // @ts-check
 
-
+const calculadora = require("../models/calculadora")
 
 test("chiando primeiro test", function(){
     console.log("oie");
 });
 
+test(" soma de 2 + 2 deve retornar 4", function(){
+  const resultado = calculadora. soma(2,2)
+  expect(resultado).toBe(4)
+
+});
+
+test ("soma de 2 + 4 deve retornar 6", function(){
+  const resultado = calculadora. soma(2,4)
+  expect(resultado).toBe(6)
+
+})
