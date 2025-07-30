1c74d883d73b5c329a4cd5cf7ae94acf5977ec8a
RomarioNaiana <romarionaiana@gmail.com>
2025-07-30 17:28:31 +0000

    fim

--- models/callback.js
+++ models/callback.js
@@ -1,4 +1,4 @@
-const { error } = require("node:console");
+
 const fs = require("node:fs");
 
 fs.readFile("./arq1.txt", function( error, resposta){
@@ -6,18 +6,21 @@ fs.readFile("./arq1.txt", function( error, resposta){
     console.log("erro ao ler o primeiro arquivo")
       return
   }
-  fs.readFile("./arq2.txt", function (error2, resposta2) {
-if(error2){
-  console.log("erro ao ler o segundo arquivo")
-  return
-}
-  fs.readFile("./arq3.txt", function (error3, resposta3) {
-    console.log("reposta", resposta.toString()+resposta2.toString()+resposta3.toString);
-if(error3){
-  console.log("erro ao ler o terceiro arquivo")
-}
 
-  });
+  fs.readFile("./arq2.txt", function (error2, resposta2) {
+    if(error2){
+      console.log("erro ao ler o segundo arquivo")
+      return
+    }
+
+    fs.readFile("./arq3.txt", function (error3, resposta3) {
+      if(error3){
+        console.log("erro ao ler o terceiro arquivo")
+        return
+      }
+      console.log(`${resposta}${resposta2}${resposta3}`);
+
+    });
   });
 });
 
