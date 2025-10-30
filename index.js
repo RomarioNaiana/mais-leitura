67b78b8895cab11926cb74659974545947c755b9
RomarioNaiana <romarionaiana@gmail.com>
2025-10-30 17:20:05 +0000

    fim

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
+require("dotenv").config();
+const livroRepository = require("./infra/repository/livro");
 
-console.log(process.env);
-const {query} = require("./infra/database");
-
-console.log(process.env.NAME);
-
-query("SELECT 1 + 1;")
-.then(function(volue) {
-  console.log(volue);
+livroRepository
+.consultarTodos()
+.then(function (resultados) {
+  console.log(resultados)
 })
-.catch(function(err) {
-  console.log("deu ruim",err);
-});
+.catch(function (error) {
+  console.log("Deu ruim", error)
+})
---
+++ infra/repository/livro.js
@@ -0,0 +1,11 @@
+const {query} = require("../database");
+
+async function consultarTodos() {
+  const sql = `SELECT * FROM livro`
+
+  const resultados = await query(sql)
+
+  return resultados;
+}
+
+module.exports = {consultarTodos}
