679d8a2359f85fede1cc27d86ad9a9651d561fb7
RomarioNaiana <romarionaiana@gmail.com>
2025-11-05 16:48:44 +0000

    ok

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
+const { port } = require("pg/lib/defaults");
 const livroRepository = require("./infra/repository/livro");
 
-livroRepository
-.consultarTodos()
-.then(function (resultados) {
-  console.log(resultados)
-})
-.catch(function (error) {
-  console.log("Deu ruim", error)
-})
+cont serve = http.creatServer(function (red, res) {
+  //res.writeHead(200, {"content-type" "text/plain"})
+  res.end("Ok vem vindo");
+});
+
+cont por = process.env.PORT || 3000;
+
+ServiceWorkerRegistration.listen(port, function () {
+   console.log("Inicializando servidor HTTP na porta " + port);
+});
